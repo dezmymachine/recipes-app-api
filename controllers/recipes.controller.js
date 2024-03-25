31875bdf00faf3abcfd0a937795cc802492c7760
dezmymachine <desmondkwakucoffie195@gmail.com>
2024-03-25 18:20:20 +0000

    add-recipe page and routing

--- controllers/recipes.controller.js
+++ controllers/recipes.controller.js
@@ -1,26 +1,59 @@
 import { Recipesmodel } from "../model/recipe.js";
 
-export const addRecipe = async (req, res) => {
-  //Add recipe to database
-  const createResult = await Recipesmodel.create(req.body);
-  //return response
-  res.status(201).json(createResult);
+export const addRecipe = async (req, res, next) => {
+  try {
+    //Add recipe to database
+    const createResult = await Recipesmodel.create(req.body);
+    //return response
+    res.status(201).json(createResult);
+  } catch (error) {
+    next(error);
+  }
 };
 export const getRecipes = async (req, res) => {
-  //get all recipes
-  const findResult = await Recipesmodel.find();
-  //return response
-  res.status(200).json(findResult);
+  try {
+    //get all recipes
+    const findResult = await Recipesmodel.find();
+    //return response
+    res.status(200).json(findResult);
+  } catch (error) {
+    next(error);
+  }
 };
 
-export const getRecipe = async (req, res) => {
-  let getOneRecipe = await Recipesmodel.findOne({ _id: req.params.id });
-  res.status(200).json(getOneRecipe);
+export const getRecipe = async (req, res, next) => {
+  // let getOneRecipe = await Recipesmodel.findOne({ _id: req.params.id });
+  // res.status(200).json(getOneRecipe);
   // res.send(`recipe with id:${req.params.id}`);
+
+  try {
+    //get single recipe by id
+    const findByIdResult = await Recipesmodel.findById(req.params.id);
+    //return 404 if no recipe is found
+    // if (findByIdResult === null) {
+    //   return res.status(404).json({
+    //     message: `Recipe with ID : ${req.params.id} not found`,
+    //   });
+    // }
+    // //return response
+    // res.status(200).json(findByIdResult);
+    if (findByIdResult === null) {
+      res.status(404).json({
+        message: `Recipe with ID : ${req.params.id} not found`,
+      });
+    } else {
+      //return response
+      res.status(200).json(findByIdResult);
+    }
+  } catch (error) {
+    next(error);
+  }
 };
 
-export const updateRecipe = (req, res) => {
-  res.send(`update recipe with id:${req.params.id}`);
+export const updateRecipe = async (req, res) => {
+  let updateOneRecipe = await Recipesmodel.updateOne({ _id: req.params.id });
+  res.status(200).json(updateOneRecipe);
+  // res.send(`update recipe with id:${req.params.id}`);
 };
 
 export const deleteRecipe = (req, res) => {
